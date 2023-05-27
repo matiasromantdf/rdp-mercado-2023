9d9c7d914dd45990dd39339b1f00aa48fa6eca99
matiasromantdf <mroman.tdf@gmail.com>
2023-05-27 00:10:44 -0300

    primer commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+
+</script>
+
+<template>
+   <div class="container">
+    <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+   
+      <RouterView />
+  </div>
+      
+  
+
+
+</template>
+
+<style scoped>
+
+</style>
