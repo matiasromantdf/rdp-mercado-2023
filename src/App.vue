6f756b34e0abb6d104de48cb8274a9e4088bf056
matiasromantdf <mroman.tdf@gmail.com>
2023-05-27 01:16:00 -0300

    fondo y navbar

--- src/App.vue
+++ src/App.vue
@@ -4,20 +4,45 @@ import { RouterLink, RouterView } from 'vue-router'
 </script>
 
 <template>
-   <div class="container">
-    <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-   
-      <RouterView />
+  <div class="container">
+    <nav class="navbar navbar-expand-lg navbar-light" style="background-color: #e3f2fd;">
+      <div class="container-fluid">
+        <router-link class="navbar-brand" to="/"><span class="material-symbols-outlined">
+            storefront
+          </span></router-link>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+          aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <router-link class="nav-link active" aria-current="page" to="/">Home</router-link>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Lista de Tiendas</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Acceso comerciantes</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+    <RouterView />
   </div>
-      
-  
-
-
 </template>
 
 <style scoped>
+.material-symbols-outlined {
+  font-size: 40px;
+  font-weight: 10;
+  border: 1px solid black;
+  border-radius: 5px;
+  background-color: #c3d9f7;
 
+}
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,19 +1,55 @@
 <template>
-  <div>
-    pagina principal
+  <div class="container">
+    <input type="text" id="txtBusqueda" placeholder="estoy buscando...">
+    <button class="boton-buscar">Buscar</button>
   </div>
 </template>
 
 <script>
 export default {
-  setup () {
-    
+  setup() {
+
 
     return {}
   }
 }
 </script>
 
-<style lang="scss" scoped>
+<style scoped>
+#txtBusqueda {
+  width: 100%;
+  height: 60px;
+  border: 1px solid rgb(168, 164, 164);
+  border-radius: 5px;
+  padding: 5px;
+  font-size: 20px;
+  max-width: 500px;
+  opacity: 0.5;
+  color: rgb(11, 5, 92);
+  box-shadow: 0px 0px 10px 0px rgb(0 0 0 / 50%);
+}
 
+.container {
+  background-image: linear-gradient(to right, #312858 0%, #a6c1ee 51%, #508fee 100%);
+  width: 100%;
+  height: 90vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.boton-buscar {
+  width: 100px;
+  height: 60px;
+  border: 1px solid rgb(168, 164, 164);
+  border-radius: 5px;
+  padding: 5px;
+  font-size: 20px;
+  max-width: 500px;
+  opacity: 0.5;
+  color: rgb(11, 5, 92);
+  box-shadow: 0px 0px 10px 0px rgb(0 0 0 / 50%);
+  background-color: #c3d9f7;
+  margin-left: 2px;
+}
 </style>
